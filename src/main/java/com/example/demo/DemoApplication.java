90d021cc2ef74391ed6d0c8aa0b413507d14c999
ch <mkmsacc@outlook.com>
2023-02-24 14:18:51 +0700

    Add hello controller

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,30 @@
 package com.example.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
+@RestController
+@RequestMapping("/")
+class DemoController {
+
+	private final Logger log = LoggerFactory.getLogger(DemoController.class);
+	@GetMapping
+	public String getHello() {
+		log.info("getHello was called");
+		return "Hello";
 	}
 
 }
